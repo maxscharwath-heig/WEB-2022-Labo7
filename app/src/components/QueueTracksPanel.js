44ab5b2e962ca6c58e1e8ac3542c75564c6687b7
Maxime Scharwath <maxscharwath@gmail.com>
2022-12-06 10:37:39 +0100

    Start logic

--- app/src/components/QueueTracksPanel.js
+++ app/src/components/QueueTracksPanel.js
@@ -1,9 +1,18 @@
-import React,{useContext} from "react";
+import React,{useContext,useEffect} from "react";
 import QueueTracksList from "./QueueTracksList";
 import {PlayerContext} from "../context/PlayerContext";
 
 function QueueTracksPanel() {
-    const {queue} = useContext(PlayerContext);
+    const {queue,addToQueue,fetchPlaylist,playNext} = useContext(PlayerContext);
+
+    useEffect(() => {
+        fetchPlaylist(1479458365).then(data => {
+            console.log(data);
+            addToQueue(...data.tracks.data);
+            playNext();
+        })
+    },[]);
+
     return (
         <>
             <div>
--- app/src/context/PlayerContext.js
+++ app/src/context/PlayerContext.js
@@ -3,31 +3,57 @@ import {createContext, useState} from "react";
 export const PlayerContext = createContext({});
 
 export const PlayerProvider = ({children}) => {
+    const [pastTracks, setPastTracks] = useState([]);
     const [queue, setQueue] = useState([]);
     const [currentTrack, setCurrentTrack] = useState(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [audio] = useState(new Audio());
 
     const fetchPlaylist = async (playlistId) => {
-        //TODO: fetch playlist from API
+        const response = await fetch(`http://localhost:8080/playlist/${playlistId}`);
+        return await response.json();
     }
 
     const fetchPopularPlaylists = async () => {
-        //TODO: fetch popular playlists from API
+        const response = await fetch("http://localhost:8080/popular/playlists");
+        return await response.json();
     }
 
-    const addToQueue = (track) => {
-        setQueue([...queue, track]);
+    const addToQueue = (...tracks) => {
+        setQueue([...queue, ...tracks]);
     }
 
     const toggleTrack = () => {
-        //TODO: toggle play
+        if (isPlaying) {
+            audio.pause();
+        } else {
+            audio.play();
+        }
+        setIsPlaying(!isPlaying);
+    }
+
+    const playTrack = (track) => {
+        setCurrentTrack(track);
+        audio.src = track.preview;
+        audio.play();
+        setIsPlaying(true);
     }
 
     const playNext = () => {
-        //TODO: play next track
+        if (queue.length > 0) {
+            const nextTrack = queue[0];
+            setQueue(queue.slice(1));
+            setPastTracks([currentTrack, ...pastTracks]);
+            playTrack(nextTrack);
+        }
     }
 
     const playPrevious = () => {
-        //TODO: play previous track
+        if (pastTracks.length > 0) {
+            const previousTrack = pastTracks[0];
+            setPastTracks(pastTracks.slice(1));
+            playTrack(previousTrack);
+        }
     }
 
 
@@ -39,6 +65,7 @@ export const PlayerProvider = ({children}) => {
             fetchPopularPlaylists,
             addToQueue,
             toggleTrack,
+            playTrack,
             playNext,
             playPrevious
         }}>
