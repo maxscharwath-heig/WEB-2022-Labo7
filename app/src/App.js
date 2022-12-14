ae56a892fe43a4f7d07e62812760f9e76bcfd594
Lazzzer <dev@lazarpavicevic.ch>
2022-12-14 20:41:04 +0100

    refactor: remove buggy suspense

--- app/src/App.js
+++ app/src/App.js
@@ -1,61 +1,49 @@
-import React, { lazy, Suspense } from 'react'
-import { Drawer, Box } from '@mui/material'
-import { PlayerProvider } from './context/PlayerContext'
-import QueueTracksPanel from './components/QueueTracksPanel'
-import Player from './components/Player'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React from "react";
+import { Drawer, Box } from "@mui/material";
+import { PlayerProvider } from "./context/PlayerContext";
+import QueueTracksPanel from "./components/QueueTracksPanel";
+import Player from "./components/Player";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PopularPlaylists from "./components/PopularPlaylists";
+import Playlist from "./components/Playlist";
 
-const Playlist = lazy(() => import('./components/Playlist'))
-const PopularPlaylists = lazy(() => import('./components/PopularPlaylists'))
-
-const drawerWidth = 330
+const drawerWidth = 330;
 function App() {
-   return (
-      <PlayerProvider>
-         <Box sx={{ display: 'flex', height: '100vh', flexDirection: 'column' }}>
-            <Box component='main' sx={{ display: 'flex', overflow: 'hidden', flex: 1 }}>
-               <Box sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-                  <BrowserRouter>
-                     <Routes>
-                        <Route
-                           path='/'
-                           element={
-                              <Suspense fallback={<div>Loading Playlists...</div>}>
-                                 <PopularPlaylists />
-                              </Suspense>
-                           }
-                        />
-                        <Route
-                           path='/playlist/:id'
-                           element={
-                              <Suspense fallback={<div>Loading Playlist...</div>}>
-                                 <Playlist />
-                              </Suspense>
-                           }
-                        />
-                     </Routes>
-                  </BrowserRouter>
-               </Box>
-               <Drawer
-                  variant='permanent'
-                  anchor='right'
-                  sx={{
-                     width: drawerWidth,
-                     [`& .MuiDrawer-paper`]: {
-                        width: drawerWidth,
-                        boxSizing: 'border-box',
-                     },
-                  }}
-               >
-                  <Box sx={{ overflow: 'auto' }}>
-                     <QueueTracksPanel />
-                  </Box>
-               </Drawer>
+  return (
+    <PlayerProvider>
+      <Box sx={{ display: "flex", height: "100vh", flexDirection: "column" }}>
+        <Box
+          component="main"
+          sx={{ display: "flex", overflow: "hidden", flex: 1 }}
+        >
+          <Box sx={{ display: "flex", flexDirection: "column", flex: 1 }}>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<PopularPlaylists />} />
+                <Route path="/playlist/:id" element={<Playlist />} />
+              </Routes>
+            </BrowserRouter>
+          </Box>
+          <Drawer
+            variant="permanent"
+            anchor="right"
+            sx={{
+              width: drawerWidth,
+              [`& .MuiDrawer-paper`]: {
+                width: drawerWidth,
+                boxSizing: "border-box",
+              },
+            }}
+          >
+            <Box sx={{ overflow: "auto" }}>
+              <QueueTracksPanel />
             </Box>
-            <Player />
-         </Box>
-      </PlayerProvider>
-   )
+          </Drawer>
+        </Box>
+        <Player />
+      </Box>
+    </PlayerProvider>
+  );
 }
 
-export default App
+export default App;
