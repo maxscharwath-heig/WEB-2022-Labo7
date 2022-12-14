8f831dbf25fb60299f93887b0719086562a5c8bb
Lazzzer <dev@lazarpavicevic.ch>
2022-12-14 21:05:11 +0100

    refactor: change button for icon button with tooltip

--- app/src/components/Player.js
+++ app/src/components/Player.js
@@ -1,132 +1,159 @@
-import { Avatar, Box, Grid, IconButton, Slider, Stack, Typography } from '@mui/material'
-import { PlayerContext } from '../context/PlayerContext'
-import { useContext, useEffect, useState } from 'react'
-import { Pause, PlayArrow, SkipNext, SkipPrevious } from '@mui/icons-material'
+import {
+  Avatar,
+  Box,
+  Grid,
+  IconButton,
+  Slider,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { PlayerContext } from "../context/PlayerContext";
+import { useContext, useEffect, useState } from "react";
+import { Pause, PlayArrow, SkipNext, SkipPrevious } from "@mui/icons-material";
 
 function FormattedTime({ duration }) {
-   if (!duration) return <small>-:-</small>
+  if (!duration) return <small>-:-</small>;
 
-   const minutes = Math.floor(duration / 60)
-      .toString()
-      .padStart(2, '0')
-   const seconds = Math.floor(duration % 60)
-      .toString()
-      .padStart(2, '0')
-   return (
-      <small>
-         {minutes}:{seconds}
-      </small>
-   )
+  const minutes = Math.floor(duration / 60)
+    .toString()
+    .padStart(2, "0");
+  const seconds = Math.floor(duration % 60)
+    .toString()
+    .padStart(2, "0");
+  return (
+    <small>
+      {minutes}:{seconds}
+    </small>
+  );
 }
 
 function PlaySlider({ currentTime, duration, setCurrentTime }) {
-   const [value, setValue] = useState(0)
-   // set value with props but keep the possibility to change it with the slider
+  const [value, setValue] = useState(0);
+  // set value with props but keep the possibility to change it with the slider
 
-   useEffect(() => {
-      setValue(currentTime)
-   }, [currentTime])
+  useEffect(() => {
+    setValue(currentTime);
+  }, [currentTime]);
 
-   function handleChange(newValue) {
-      setCurrentTime(newValue)
-      setValue(newValue)
-   }
+  function handleChange(newValue) {
+    setCurrentTime(newValue);
+    setValue(newValue);
+  }
 
-   return (
-      <>
-         <FormattedTime duration={currentTime ?? 0} />
-         <Slider
-            size='small'
-            min={0}
-            max={duration || 0}
-            value={value}
-            onChange={(e, newValue) => handleChange(newValue)}
-         />
-         <FormattedTime duration={duration ?? 0} />
-      </>
-   )
+  return (
+    <>
+      <FormattedTime duration={currentTime ?? 0} />
+      <Slider
+        size="small"
+        min={0}
+        max={duration || 0}
+        value={value}
+        onChange={(e, newValue) => handleChange(newValue)}
+      />
+      <FormattedTime duration={duration ?? 0} />
+    </>
+  );
 }
 
 const Player = () => {
-   const { audioState, currentTrack, toggleTrack, playPrevious, playNext, setCurrentTime, queue } =
-      useContext(PlayerContext)
+  const {
+    audioState,
+    currentTrack,
+    toggleTrack,
+    playPrevious,
+    playNext,
+    setCurrentTime,
+    queue,
+  } = useContext(PlayerContext);
 
-   const buttonsActive = () => {
-      return currentTrack != null || (!currentTrack && queue.length > 0)
-   }
+  const buttonsActive = () => {
+    return currentTrack != null || (!currentTrack && queue.length > 0);
+  };
 
-   return (
-      <Box
-         sx={{
-            border: 1,
-            borderColor: 'lightgray',
-            backgroundColor: 'white',
-            zIndex: (theme) => theme.zIndex.drawer + 1,
-            width: '100%',
-         }}
-      >
-         <Grid container alignItems='center' sx={{ height: 75 }}>
-            <Grid item xs={4}>
-               {currentTrack && (
-                  <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                     <Avatar
-                        src={currentTrack.album.cover_medium}
-                        alt='trackCover'
-                        variant='square'
-                        sx={{ height: 75, width: 'auto' }}
-                     />
-                     <Box sx={{ ml: 1.5, minWidth: 0 }}>
-                        <Typography variant='caption' fontWeight={500}>
-                           {currentTrack?.artist.name}
-                        </Typography>
-                        <Typography noWrap>{currentTrack?.title}</Typography>
-                     </Box>
-                  </Box>
-               )}
-            </Grid>
+  return (
+    <Box
+      sx={{
+        borderTop: 1,
+        borderColor: "lightgray",
+        backgroundColor: "white",
+        zIndex: (theme) => theme.zIndex.drawer + 1,
+        width: "100%",
+      }}
+    >
+      <Grid container alignItems="center" sx={{ height: 75 }}>
+        <Grid item xs={4}>
+          {currentTrack && (
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Avatar
+                src={currentTrack.album.cover_medium}
+                alt="trackCover"
+                variant="square"
+                sx={{ height: 75, width: "auto" }}
+              />
+              <Box sx={{ ml: 1.5, minWidth: 0 }}>
+                <Typography variant="caption" fontWeight={500}>
+                  {currentTrack?.artist.name}
+                </Typography>
+                <Typography noWrap>{currentTrack?.title}</Typography>
+              </Box>
+            </Box>
+          )}
+        </Grid>
 
-            <Grid item xs={4}>
-               <Box>
-                  <Stack spacing={2} direction='row' justifyContent='center' alignItems='center'>
-                     <IconButton
-                        onClick={() => {
-                           playPrevious()
-                        }}
-                        disabled={!buttonsActive()}
-                     >
-                        <SkipPrevious />
-                     </IconButton>
-                     <IconButton
-                        onClick={() => {
-                           toggleTrack()
-                        }}
-                        disabled={!buttonsActive()}
-                     >
-                        {audioState.isPlaying ? <Pause /> : <PlayArrow />}
-                     </IconButton>
-                     <IconButton
-                        onClick={() => {
-                           playNext()
-                        }}
-                        disabled={!buttonsActive()}
-                     >
-                        <SkipNext />
-                     </IconButton>
+        <Grid item xs={4}>
+          <Box>
+            <Stack
+              spacing={2}
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <IconButton
+                onClick={() => {
+                  playPrevious();
+                }}
+                disabled={!buttonsActive()}
+              >
+                <SkipPrevious />
+              </IconButton>
+              <IconButton
+                onClick={() => {
+                  toggleTrack();
+                }}
+                disabled={!buttonsActive()}
+              >
+                {audioState.isPlaying ? <Pause /> : <PlayArrow />}
+              </IconButton>
+              <IconButton
+                onClick={() => {
+                  playNext();
+                }}
+                disabled={!buttonsActive()}
+              >
+                <SkipNext />
+              </IconButton>
 
-                     {currentTrack && <PlaySlider setCurrentTime={setCurrentTime} {...audioState} />}
-                  </Stack>
-               </Box>
-            </Grid>
-            <Grid item xs={4} paddingRight={2}>
-               <Box textAlign='right'>
-                  Powered by
-                  <br />
-                  <img src='/deezer-logo.svg' alt='Deezer logo' id='footer-logo' height={20} />
-               </Box>
-            </Grid>
-         </Grid>
-      </Box>
-   )
-}
+              {currentTrack && (
+                <PlaySlider setCurrentTime={setCurrentTime} {...audioState} />
+              )}
+            </Stack>
+          </Box>
+        </Grid>
+        <Grid item xs={4} paddingRight={2}>
+          <Box textAlign="right">
+            Powered by
+            <br />
+            <img
+              src="/deezer-logo.svg"
+              alt="Deezer logo"
+              id="footer-logo"
+              height={20}
+            />
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
 
-export default Player
+export default Player;
--- app/src/components/Playlist.js
+++ app/src/components/Playlist.js
@@ -1,122 +1,146 @@
-import { Box, Button, Divider, List, ListItem, ListItemButton, ListItemText, Skeleton, Typography } from '@mui/material'
-import { ArrowBack } from '@mui/icons-material'
-import SongRow from './SongRow'
-import React, { useContext, useEffect, useState } from 'react'
-import QueueMusicIcon from '@mui/icons-material/QueueMusic'
-import Page from './Page'
-import { PlayerContext } from '../context/PlayerContext'
-import { useNavigate, useParams } from 'react-router-dom'
+import {
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Skeleton,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { ArrowBack } from "@mui/icons-material";
+import SongRow from "./SongRow";
+import React, { useContext, useEffect, useState } from "react";
+import QueueMusicIcon from "@mui/icons-material/QueueMusic";
+import Page from "./Page";
+import { PlayerContext } from "../context/PlayerContext";
+import { useNavigate, useParams } from "react-router-dom";
 
 const fetchPlaylist = async (playlistId) => {
-   const response = await fetch(`http://localhost:8080/playlist/${playlistId}`)
-   return await response.json()
-}
+  const response = await fetch(`http://localhost:8080/playlist/${playlistId}`);
+  return await response.json();
+};
 
 export default function Playlist() {
-   const { addToQueue, playTrack } = useContext(PlayerContext)
-   const [playlist, setPlaylist] = useState(null)
-   const navigate = useNavigate()
-   const { id } = useParams()
-   useEffect(() => {
-      fetchPlaylist(id).then(setPlaylist).catch(console.error)
-   }, [id])
+  const { addToQueue, playTrack } = useContext(PlayerContext);
+  const [playlist, setPlaylist] = useState(null);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  useEffect(() => {
+    fetchPlaylist(id).then(setPlaylist).catch(console.error);
+  }, [id]);
 
-   function handleAddToQueue(e, track) {
-      e.stopPropagation()
-      addToQueue(track)
-   }
+  function handleAddToQueue(e, track) {
+    e.stopPropagation();
+    addToQueue(track);
+  }
 
-   function handleClickTrack(e, track) {
-      e.stopPropagation()
-      playTrack(track)
-   }
+  function handleClickTrack(e, track) {
+    e.stopPropagation();
+    playTrack(track);
+  }
 
-   const creationYear = () => {
-      if (playlist) {
-         return new Date(playlist.creation_date).getFullYear()
+  const creationYear = () => {
+    if (playlist) {
+      return new Date(playlist.creation_date).getFullYear();
+    }
+    return "";
+  };
+
+  const playlistDurationMinutes = () => {
+    if (playlist) {
+      return Math.ceil(playlist.duration / 60);
+    }
+    return "";
+  };
+
+  return (
+    <Page
+      toolbar={
+        <Button
+          variant="plain"
+          startIcon={<ArrowBack />}
+          size="small"
+          onClick={() => navigate("/")}
+        >
+          Back to playlists
+        </Button>
       }
-      return ''
-   }
-
-   const playlistDurationMinutes = () => {
-      if (playlist) {
-         return Math.ceil(playlist.duration / 60)
-      }
-      return ''
-   }
-
-   return (
-      <Page
-         toolbar={
-            <Button variant='plain' startIcon={<ArrowBack />} size='small' onClick={() => navigate('/')}>
-               Back to playlists
-            </Button>
-         }
-      >
-         <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-            <Box
-               sx={{
-                  display: 'flex',
-               }}
-            >
-               {playlist ? (
-                  <img src={playlist.picture_medium} alt='playlistCover' />
-               ) : (
-                  <Skeleton variant='rectangular' width={250} height={250} />
-               )}
-               <Box
-                  sx={{
-                     paddingLeft: 2,
-                  }}
-               >
-                  <Typography
-                     variant='body2'
-                     sx={{
-                        textTransform: 'uppercase',
-                     }}
+    >
+      <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
+        <Box
+          sx={{
+            display: "flex",
+          }}
+        >
+          {playlist ? (
+            <img src={playlist.picture_medium} alt="playlistCover" />
+          ) : (
+            <Skeleton variant="rectangular" width={250} height={250} />
+          )}
+          <Box
+            sx={{
+              paddingLeft: 2,
+            }}
+          >
+            <h3>Playlist</h3>
+            <Typography variant="h4">
+              {playlist ? playlist.title : <Skeleton width={200} />}
+            </Typography>
+            <small>
+              {playlist ? (
+                `${creationYear()} · ${
+                  playlist?.nb_tracks
+                } tracks · TODO minutes`
+              ) : (
+                <Skeleton width={200} />
+              )}
+            </small>
+          </Box>
+        </Box>
+        <List sx={{ overflow: "auto", flex: 1, mt: 2 }}>
+          {playlist
+            ? playlist.tracks.data.map((track) => (
+                <React.Fragment key={track.id}>
+                  <SongRow
+                    track={track}
+                    onClick={(e) => handleClickTrack(e, track)}
                   >
-                     Playlist
-                  </Typography>
-                  <Typography variant='h4'>{playlist ? playlist.title : <Skeleton width={200} />}</Typography>
-                  <small>{playlist ? `By ${playlist?.creator.name}` : <Skeleton width={200} />}</small>
-                  <br></br>
-                  <small>
-                     {playlist ? (
-                        `${creationYear()} · ${playlist?.nb_tracks} tracks · ${playlistDurationMinutes()} minutes`
-                     ) : (
-                        <Skeleton width={200} />
-                     )}
-                  </small>
-               </Box>
-            </Box>
-            <List sx={{ overflow: 'auto', flex: 1, mt: 2 }}>
-               {playlist
-                  ? playlist.tracks.data.map((track) => (
-                       <React.Fragment key={track.id}>
-                          <SongRow track={track} onClick={(e) => handleClickTrack(e, track)}>
-                             <Button
-                                variant='outlined'
-                                startIcon={<QueueMusicIcon />}
-                                size='small'
-                                onClick={(e) => handleAddToQueue(e, track)}
-                             >
-                                Add to queue
-                             </Button>
-                          </SongRow>
-                          <Divider component='li' />
-                       </React.Fragment>
-                    ))
-                  : // 10 Skeletons for 10 tracks
-                    [...Array(10)].map((_, index) => (
-                       <ListItemButton key={index}>
-                          <ListItem>
-                             <Skeleton variant='rectangular' height={40} width={40} sx={{ marginRight: '1rem' }} />
-                             <ListItemText primary={<Skeleton width={200} />} secondary={<Skeleton width={100} />} />
-                          </ListItem>
-                       </ListItemButton>
-                    ))}
-            </List>
-         </Box>
-      </Page>
-   )
+                    <Tooltip title="Add song to queue" placement="top">
+                      <IconButton
+                        aria-label="add to queue"
+                        size="small"
+                        onClick={(e) => handleAddToQueue(e, track)}
+                      >
+                        <QueueMusicIcon />
+                      </IconButton>
+                    </Tooltip>
+                  </SongRow>
+                  <Divider component="li" />
+                </React.Fragment>
+              ))
+            : // 10 Skeletons for 10 tracks
+              [...Array(10)].map((_, index) => (
+                <ListItemButton key={index}>
+                  <ListItem>
+                    <Skeleton
+                      variant="rectangular"
+                      height={40}
+                      width={40}
+                      sx={{ marginRight: "1rem" }}
+                    />
+                    <ListItemText
+                      primary={<Skeleton width={200} />}
+                      secondary={<Skeleton width={100} />}
+                    />
+                  </ListItem>
+                </ListItemButton>
+              ))}
+        </List>
+      </Box>
+    </Page>
+  );
 }
