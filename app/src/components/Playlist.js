b66f5698183068addc645cb666a5602cc4885792
Lazzzer <dev@lazarpavicevic.ch>
2022-12-14 20:37:56 +0100

    feat: add skeleton for popular playlists

--- app/src/components/Playlist.js
+++ app/src/components/Playlist.js
@@ -1,107 +1,136 @@
-import { Box, Button, Divider, List, ListItem, ListItemButton, ListItemText, Skeleton, Typography } from '@mui/material'
-import { ArrowBack } from '@mui/icons-material'
-import SongRow from './SongRow'
-import React, { useContext, useEffect, useState } from 'react'
-import QueueMusicIcon from '@mui/icons-material/QueueMusic'
-import Page from './Page'
-import { PlayerContext } from '../context/PlayerContext'
-import { useNavigate, useParams } from 'react-router-dom'
+import {
+  Box,
+  Button,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Skeleton,
+  Typography,
+} from "@mui/material";
+import { ArrowBack } from "@mui/icons-material";
+import SongRow from "./SongRow";
+import React, { useContext, useEffect, useState } from "react";
+import QueueMusicIcon from "@mui/icons-material/QueueMusic";
+import Page from "./Page";
+import { PlayerContext } from "../context/PlayerContext";
+import { useNavigate, useParams } from "react-router-dom";
 
 const fetchPlaylist = async (playlistId) => {
-   const response = await fetch(`http://localhost:8080/playlist/${playlistId}`)
-   await new Promise((resolve) => setTimeout(resolve, 1000))
-   return await response.json()
-}
+  const response = await fetch(`http://localhost:8080/playlist/${playlistId}`);
+  return await response.json();
+};
 
 export default function Playlist() {
-   const { addToQueue, playTrack } = useContext(PlayerContext)
-   const [playlist, setPlaylist] = useState(null)
-   const navigate = useNavigate()
-   const { id } = useParams()
-   useEffect(() => {
-      fetchPlaylist(id).then(setPlaylist).catch(console.error)
-   }, [id])
+  const { addToQueue, playTrack } = useContext(PlayerContext);
+  const [playlist, setPlaylist] = useState(null);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  useEffect(() => {
+    fetchPlaylist(id).then(setPlaylist).catch(console.error);
+  }, [id]);
 
-   function handleAddToQueue(e, track) {
-      e.stopPropagation()
-      addToQueue(track)
-   }
+  function handleAddToQueue(e, track) {
+    e.stopPropagation();
+    addToQueue(track);
+  }
 
-   function handleClickTrack(e, track) {
-      e.stopPropagation()
-      playTrack(track)
-   }
+  function handleClickTrack(e, track) {
+    e.stopPropagation();
+    playTrack(track);
+  }
 
-   const creationYear = () => {
-      if (playlist) {
-         return new Date(playlist.creation_date).getFullYear()
+  const creationYear = () => {
+    if (playlist) {
+      return new Date(playlist.creation_date).getFullYear();
+    }
+    return "";
+  };
+
+  return (
+    <Page
+      toolbar={
+        <Button
+          variant="plain"
+          startIcon={<ArrowBack />}
+          size="small"
+          onClick={() => navigate("/")}
+        >
+          Back to playlists
+        </Button>
       }
-      return ''
-   }
-
-   return (
-      <Page
-         toolbar={
-            <Button variant='plain' startIcon={<ArrowBack />} size='small' onClick={() => navigate('/')}>
-               Back to playlists
-            </Button>
-         }
-      >
-         <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-            <Box
-               sx={{
-                  display: 'flex',
-               }}
-            >
-               {playlist ? (
-                  <img src={playlist.picture_medium} alt='playlistCover' />
-               ) : (
-                  <Skeleton variant='rectangular' width={250} height={250} />
-               )}
-               <Box
-                  sx={{
-                     paddingLeft: 2,
-                  }}
-               >
-                  <h3>Playlist</h3>
-                  <Typography variant='h4'>{playlist ? playlist.title : <Skeleton width={200} />}</Typography>
-                  <small>
-                     {playlist ? (
-                        `${creationYear()} · ${playlist?.nb_tracks} tracks · TODO minutes`
-                     ) : (
-                        <Skeleton width={200} />
-                     )}
-                  </small>
-               </Box>
-            </Box>
-            <List sx={{ overflow: 'auto', flex: 1, mt: 2 }}>
-               {playlist
-                  ? playlist.tracks.data.map((track) => (
-                       <React.Fragment key={track.id}>
-                          <SongRow track={track} onClick={(e) => handleClickTrack(e, track)}>
-                             <Button
-                                variant='outlined'
-                                startIcon={<QueueMusicIcon />}
-                                size='small'
-                                onClick={(e) => handleAddToQueue(e, track)}
-                             >
-                                Add to queue
-                             </Button>
-                          </SongRow>
-                          <Divider component='li' />
-                       </React.Fragment>
-                    ))
-                  : // 10 Skeletons for 10 tracks
-                    [...Array(10)].map((_, index) => (
-                       <ListItemButton key={index}>
-                          <ListItem>
-                             <Skeleton variant='rectangular' height={40} width={40} sx={{ marginRight: '1rem' }} />
-                             <ListItemText primary={<Skeleton width={200} />} secondary={<Skeleton width={100} />} />
-                          </ListItem>
-                       </ListItemButton>
-                    ))}
-            </List>
-         </Box>
-      </Page>
-   )
+    >
+      <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
+        <Box
+          sx={{
+            display: "flex",
+          }}
+        >
+          {playlist ? (
+            <img src={playlist.picture_medium} alt="playlistCover" />
+          ) : (
+            <Skeleton variant="rectangular" width={250} height={250} />
+          )}
+          <Box
+            sx={{
+              paddingLeft: 2,
+            }}
+          >
+            <h3>Playlist</h3>
+            <Typography variant="h4">
+              {playlist ? playlist.title : <Skeleton width={200} />}
+            </Typography>
+            <small>
+              {playlist ? (
+                `${creationYear()} · ${
+                  playlist?.nb_tracks
+                } tracks · TODO minutes`
+              ) : (
+                <Skeleton width={200} />
+              )}
+            </small>
+          </Box>
+        </Box>
+        <List sx={{ overflow: "auto", flex: 1, mt: 2 }}>
+          {playlist
+            ? playlist.tracks.data.map((track) => (
+                <React.Fragment key={track.id}>
+                  <SongRow
+                    track={track}
+                    onClick={(e) => handleClickTrack(e, track)}
+                  >
+                    <Button
+                      variant="outlined"
+                      startIcon={<QueueMusicIcon />}
+                      size="small"
+                      onClick={(e) => handleAddToQueue(e, track)}
+                    >
+                      Add to queue
+                    </Button>
+                  </SongRow>
+                  <Divider component="li" />
+                </React.Fragment>
+              ))
+            : // 10 Skeletons for 10 tracks
+              [...Array(10)].map((_, index) => (
+                <ListItemButton key={index}>
+                  <ListItem>
+                    <Skeleton
+                      variant="rectangular"
+                      height={40}
+                      width={40}
+                      sx={{ marginRight: "1rem" }}
+                    />
+                    <ListItemText
+                      primary={<Skeleton width={200} />}
+                      secondary={<Skeleton width={100} />}
+                    />
+                  </ListItem>
+                </ListItemButton>
+              ))}
+        </List>
+      </Box>
+    </Page>
+  );
 }
